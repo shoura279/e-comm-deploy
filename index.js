74952251b2612befe09d342ff08de6e7d60d62f6
shoura279 <ahmedshoura279@gmail.com>
2024-08-20 19:01:08 +0300

    add web hook

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 import express from "express";
 import { connectDB } from "./db/connection.js";
 import * as allRouters from "./src/index.js";
-import { globalErrorHandling } from "./src/utils/appError.js";
+import { asyncHandler, globalErrorHandling } from "./src/utils/appError.js";
 import dotenv from 'dotenv'
 import path from 'path'
+import { Cart, Order, Product, User } from "./db/index.js";
 // create server
 dotenv.config({ path: path.resolve('./config/.env') })
 const app = express();
@@ -12,6 +13,30 @@ const port = process.env.PORT || 3000;
 // connect to db
 connectDB();
 // parse req
+app.post('/webhook',
+  express.raw({ type: 'application/json' }),
+  asyncHandler(async (req, res) => {
+    const sig = req.headers['stripe-signature'].toString();
+
+    let event = stripe.webhooks.constructEvent(req.body, sig, 'whsec_e72fa9c2231c951259b2a7f9f2a2da8b2490627802ba47934339301e2067b3d8');
+
+    if (event.type == 'checkout.session.completed') {
+      const checkout = event.data.object;
+      const orderId = checkout.metaData.orderId;
+      const orderExist = await Order.findByIdAndUpdate(orderId, { status: 'placed' }, { new: true })
+      await Cart.findOneAndUpdate({ user: orderExist.user }, { products: [] }, { new: true })
+      for (const product of orderExist.products) {
+        await Product.findByIdAndUpdate(product.productId, { $inc: { stock: -product.quantity } })
+      }
+      // clear cart
+      // update order status placed
+      // product stock
+    }
+
+
+    // Return a 200 res to acknowledge receipt of the event
+    res.send();
+  }));
 app.use(express.json());
 app.use("/category", allRouters.categoryRouter);
 app.use("/sub-category", allRouters.subcategoryRouter);
